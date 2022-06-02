62803fe7161b30069d5978ecd8a2280556664d5f
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-06-02 13:04:30 +0800

    whitenoise added

--- ArtPortfolio/settings.py
+++ ArtPortfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+
 
 MEDIA_URL = 'media/'
 
