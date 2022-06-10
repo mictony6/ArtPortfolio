04dcd5efbc5389d761a0e52d1fab3e912e797c0d
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-06-10 17:24:15 +0800

    https for azure

--- ArtPortfolio/settings.py
+++ ArtPortfolio/settings.py
@@ -135,5 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = [
-    "127.0.0.1", "mic-artportfolio.herokuapp.com", "mictony.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://mictony.azurewebsites.net"]
