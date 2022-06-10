ca3b1cc6059d257f5bf1c9f1b0956dc559b26965
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-06-10 17:17:30 +0800

    csrf token for azure

--- ArtPortfolio/settings.py
+++ ArtPortfolio/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "127.0.0.1", "mic-artportfolio.herokuapp.com", "mictony.azurewebsites.net"]
